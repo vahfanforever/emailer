5dd621d3ca84719fb74a848315b8f395c01038e9
grei <vahfan@gmail.com>
2023-02-07 00:21:48 +1300

    emailer

--- birthdays/main.py
+++ birthdays/main.py
@@ -1,5 +1,6 @@
 import gspread
 import smtplib
+import json
 from oauth2client.service_account import ServiceAccountCredentials
 from datetime import datetime, timedelta
 
@@ -8,12 +9,21 @@ scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/au
 credentials = ServiceAccountCredentials.from_json_keyfile_name('/home/vahfanforever/source/emailer/birthdays/credentials.json', scope)
 sheet_name = "birthdays"
 
+with open("/home/vahfanforever/source/emailer/birthdays/email_credentials.json", 'r') as file:
+    data = json.load(file)
+
+email = data["address"]
+password = data["password"]
+
 # Connect to the Google Sheet
 gc = gspread.authorize(credentials)
 sheet = gc.open(sheet_name).sheet1
 
 # Retrieve the data from the Google Sheet
 data = sheet.get_all_records()
+
+
+birthdays = {}
 
 # Check each row of data for the specified date
 for row in data:
@@ -27,9 +37,15 @@ for row in data:
     difference = date - today
     
     # If the difference is equal to 7, send an email
-    if difference == timedelta(days=7):
-        server = smtplib.SMTP('smtp.gmail.com', 587)
-        server.starttls()
-        server.login("youremail@gmail.com", "yourpassword")
-        
-        message = "Subject: Reminder\n\nDear {0},\n\nThis is a reminder that your event is coming up in 7 days.\n\nBest"
+    if difference <= timedelta(days=14):
+        info = {}
+        info["date"] = date
+        info["time_until"] = difference
+        birthdays[name] = info
+
+if birthdays:
+    message = [f"oi u fat fuck it's {name}'s birthday on: {info['date']} -- that's in {info['time_until']} days!!!" for name, info in birthdays.items()]
+    connection = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+    connection.login(email, password)
+    connection.sendmail(from_addr=email, to_addrs=email, msg='\n'.join(message))
+    connection.close()
