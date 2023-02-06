9acc97f9951360c3d7500dc7140cc9331a8b8a36
grei <vahfan@gmail.com>
2023-02-07 01:05:30 +1300

    yassified

--- birthdays/main.py
+++ birthdays/main.py
@@ -3,6 +3,9 @@ import smtplib
 import json
 from oauth2client.service_account import ServiceAccountCredentials
 from datetime import datetime, timedelta
+from email.message import EmailMessage
+import emoji
+import calendar
 
 # Define the credentials file and the sheet name
 scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
@@ -27,25 +30,33 @@ birthdays = {}
 
 # Check each row of data for the specified date
 for row in data:
-    date = row['Date']
+    raw_date = row['Date']
     name = row['Name']
     
     # Parse the date string and compare to today's date
-    date = datetime.strptime(date, '%d-%m').date()
-    date = date.replace(year=2023)
+    raw_date = datetime.strptime(raw_date, '%d-%m').date()
+    bday_date = raw_date.replace(year=2023)
     today = datetime.now().date()
-    difference = date - today
+    difference = bday_date - today
     
     # If the difference is equal to 7, send an email
     if difference <= timedelta(days=14):
         info = {}
-        info["date"] = date
-        info["time_until"] = difference
+        info["month"] = bday_date.strftime("%B")
+        info["day"] = bday_date.strftime("%A")
+        info["date"] = bday_date.strftime("%d")
+        info["time_until"] = difference.days
         birthdays[name] = info
 
 if birthdays:
-    message = [f"oi u fat fuck it's {name}'s birthday on: {info['date']} -- that's in {info['time_until']} days!!!" for name, info in birthdays.items()]
-    connection = smtplib.SMTP_SSL('smtp.gmail.com', 465)
-    connection.login(email, password)
-    connection.sendmail(from_addr=email, to_addrs=email, msg='\n'.join(message))
-    connection.close()
+    for name, info in birthdays.items():
+        msg = EmailMessage()
+        msg.set_content(f"\nOi you fuck it's {name}'s birthday on {info['day']} {info['date']} of {info['month']} which is in {info['time_until']} day(s)")
+        msg['Subject'] = emoji.emojize(':warning: birthday reminder :warning:')
+        msg['From'] = email
+        msg['To'] = email
+
+        connection = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+        connection.login(email, password)
+        connection.send_message(msg)
+        connection.close()
